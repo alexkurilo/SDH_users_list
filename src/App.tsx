a5084090099f7325847cc42340c6cfe4dd92c41d
Oleksii <oleksii@MacBook-Pro-Aleksej.local>
2022-08-31 00:21:43 +0300

    create user info page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { UsersList } from './pages/UsersList'
+import { UserInfo } from './pages/UserInfo'
+import { useActions } from './hooks/useActions'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
+  const { getUsers, deleteUser } = useActions()
+    useEffect(() => { getUsers() }, [])
+    
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<UsersList />} />
+                    <Route path="/user/:id/info" element={<UserInfo />} />
                 </Routes>
             </BrowserRouter>
         </div>
